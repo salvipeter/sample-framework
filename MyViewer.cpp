35acc183dcd97b1d4f46f0528c31d8120615c089
Peter Salvi <vukung@yahoo.com>
2019-04-16 14:40:59 +0200

    HSV-based color map with constant saturation & value.

--- MyViewer.cpp
+++ MyViewer.cpp
@@ -234,14 +234,36 @@ void MyViewer::updateMeanCurvature(bool update_min_max) {
 }
 #endif
 
+static Vec HSV2RGB(Vec hsv) {
+  // As in Wikipedia
+  double c = hsv[2] * hsv[1];
+  double h = hsv[0] / 60;
+  double x = c * (1 - std::abs(std::fmod(h, 2) - 1));
+  double m = hsv[2] - c;
+  Vec rgb(m, m, m);
+  if (h <= 1)
+    return rgb + Vec(c, x, 0);
+  if (h <= 2)
+    return rgb + Vec(x, c, 0);
+  if (h <= 3)
+    return rgb + Vec(0, c, x);
+  if (h <= 4)
+    return rgb + Vec(0, x, c);
+  if (h <= 5)
+    return rgb + Vec(x, 0, c);
+  if (h <= 6)
+    return rgb + Vec(c, 0, x);
+  return rgb;
+}
+
 Vec MyViewer::meanMapColor(double d) const {
-  static const Vec red(1,0,0), green(0,1,0), blue(0,0,1);
+  double red = 0, green = 120, blue = 240; // Hue
   if (d < 0) {
     double alpha = mean_min ? std::min(d / mean_min, 1.0) : 1.0;
-    return green * (1 - alpha) + blue * alpha;
+    return HSV2RGB({green * (1 - alpha) + blue * alpha, 1, 1});
   }
   double alpha = mean_max ? std::min(d / mean_max, 1.0) : 1.0;
-  return green * (1 - alpha) + red * alpha;
+  return HSV2RGB({green * (1 - alpha) + red * alpha, 1, 1});
 }
 
 void MyViewer::fairMesh() {
